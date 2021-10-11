3c0d5cdca6554bd8ff4cc8d538f1a76dc305129c
xipaja <xipaja@gmail.com>
2021-10-11 15:06:19 -0700

    adding sliders and button for color wheel - to be used to adjust imported items position in space and its color

--- casitaBuilder/casitaUI.py
+++ casitaBuilder/casitaUI.py
@@ -1,6 +1,8 @@
 from .casitaLibrary import CasitaLibrary
 from PySide2 import QtWidgets, QtCore, QtGui
 from maya import cmds
+import pymel.core as pymel
+import pprint
 
 class CasitaUI(QtWidgets.QDialog):
     """
@@ -12,7 +14,7 @@ class CasitaUI(QtWidgets.QDialog):
 
         self.setWindowTitle('Casita Builder')
 
-        # Store instance of our files library in theUI
+        # Points to instance of our files library
         self.library = CasitaLibrary()
 
         # Every time we create a new UI instance, build and populate it
@@ -20,8 +22,26 @@ class CasitaUI(QtWidgets.QDialog):
         self.populate()
 
     def buildUI(self):
-        layout = QtWidgets.QVBoxLayout(self)
+        """
+        Build out UI with Qt 
+        """
 
+        # Main layout
+        layout = QtWidgets.QVBoxLayout(self)
+        
+        # Gallery of items that can be imported
+        self.listWidget = QtWidgets.QListWidget()
+        self.listWidget.setViewMode(QtWidgets.QListWidget.IconMode)
+        self.listWidget.setIconSize(QtCore.QSize(100, 100))
+
+        # Responsive grid UI
+        self.listWidget.setResizeMode(QtWidgets.QListWidget.Adjust)
+
+        # Buffer space between grid items
+        self.listWidget.setGridSize(QtCore.QSize(112, 200))
+
+        layout.addWidget(self.listWidget)
+        # Horizontal three buttons layout at the top
         btnWidget = QtWidgets.QWidget()
         btnLayout = QtWidgets.QHBoxLayout(btnWidget)
         layout.addWidget(btnWidget)
@@ -38,19 +58,40 @@ class CasitaUI(QtWidgets.QDialog):
         closeBtn.clicked.connect(self.close)
         btnLayout.addWidget(closeBtn)
 
-        # Gallery of items that can be imported
-        self.listWidget = QtWidgets.QListWidget()
-        self.listWidget.setViewMode(QtWidgets.QListWidget.IconMode)
-        self.listWidget.setIconSize(QtCore.QSize(100, 100))
+        # Edit item section - position sliders and color wheel
+        editWidget = QtWidgets.QWidget()
+        editLayout = QtWidgets.QHBoxLayout(editWidget)
+        layout.addWidget(editWidget)
 
-        # Responsive grid UI
-        self.listWidget.setResizeMode(QtWidgets.QListWidget.Adjust)
+        sliderWidget = QtWidgets.QWidget()
+        sliderLayout = QtWidgets.QVBoxLayout(sliderWidget)
+        editLayout.addWidget(sliderWidget)
 
-        # Buffer space between grid items
-        self.listWidget.setGridSize(QtCore.QSize(112, 200))
+        # Sliders for position of items once loaded
+        xLabel = QtWidgets.QLabel("X Position")
+        xSlider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        xSlider.setMinimum(-100)
+        xSlider.setMaximum(100)
+        xSlider.setGeometry(0, 0, 50, 30)
+        sliderLayout.addWidget(xLabel)
+        sliderLayout.addWidget(xSlider)
+        yLabel = QtWidgets.QLabel("Y Position")
+        sliderLayout.addWidget(yLabel)
+        ySlider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        ySlider.setGeometry(0, 0, 50, 30)
+        sliderLayout.addWidget(ySlider)
+        zLabel = QtWidgets.QLabel("Z Position")
+        sliderLayout.addWidget(zLabel)
+        zSlider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        zSlider.setGeometry(0, 0, 50, 30)
+        sliderLayout.addWidget(zSlider)
 
-        layout.addWidget(self.listWidget)
+        # Color button
+        self.colorBtn = QtWidgets.QPushButton()
+        self.colorBtn.clicked.connect(self.setColor)
+        editLayout.addWidget(self.colorBtn)
 
+        # Save textfield and button layout
         saveWidget = QtWidgets.QWidget()
         saveLayout = QtWidgets.QHBoxLayout(saveWidget)
         layout.addWidget(saveWidget)
@@ -62,13 +103,25 @@ class CasitaUI(QtWidgets.QDialog):
         saveBtn.clicked.connect(self.save)
         saveLayout.addWidget(saveBtn)
 
+    def setColor(self):
+            # opens up Maya color's editor and we can choose colors there
+            color = pymel.colorEditor(rgbValue = (0,1,0))
+
+            # converting our colors to floats to get around Maya's annoying returning colors in string format instead of list
+            r, g, b, a = [float(c) for c in color.split()]
+            
+            color = (r, g, b)
 
     def populate(self):
+        """
+        Clear gallery and repopulate with contents of library
+        """
         # Clear out list before finding 
         self.listWidget.clear()
 
         self.library.find()
 
+        # Add items from info dictionary to gallery
         for name, info in self.library.items():
             item = QtWidgets.QListWidgetItem(name)
             self.listWidget.addItem(item)
@@ -78,6 +131,9 @@ class CasitaUI(QtWidgets.QDialog):
             if screenshot:
                 icon = QtGui.QIcon(screenshot)
                 item.setIcon(icon)
+            
+            # Hover over item in grid for info
+            item.setToolTip(pprint.pformat(info))
 
     def load(self):
 
@@ -91,6 +147,9 @@ class CasitaUI(QtWidgets.QDialog):
         self.library.load(fileName)
 
     def save(self):
+        """
+        Saves item with user given file name
+        """
 
         fileName = self.saveTextField.text()
         print("file name", fileName)
@@ -106,6 +165,11 @@ class CasitaUI(QtWidgets.QDialog):
         self.saveTextField.setText('')
 
 def showUI():
+    """
+    Shows UI
+
+    Returns: QDialog
+    """
     ui = CasitaUI()
     ui.show()
     return ui
